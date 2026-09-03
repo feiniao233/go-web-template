3fdfee0ad29361387770d147a259687e81a0f775
feiniao233 <38801933+feiniao233@users.noreply.github.com>
2026-09-03 11:53:47 +0800

    test(redisstream): cover handler error option

--- internal/platform/redisstream/consumer_test.go
+++ internal/platform/redisstream/consumer_test.go
@@ -1,13 +1,30 @@
 package redisstream
 
 import (
+	"context"
+	"errors"
 	"testing"
 	"time"
 
+	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/require"
 )
 
 func TestRejectsInvalidSettings(t *testing.T) {
 	_, err := New(nil, "events", "workers", "worker-1", time.Second, 10)
 	require.Error(t, err)
+}
+
+func TestHandlerErrorOption(t *testing.T) {
+	client := redis.NewClient(&redis.Options{Addr: "127.0.0.1:1"})
+	defer client.Close()
+
+	called := false
+	consumer, err := New(client, "events", "workers", "worker-1", time.Second, 10, WithHandlerError(func(_ context.Context, message redis.XMessage, err error) {
+		called = message.ID == "1-0" && errors.Is(err, context.Canceled)
+	}))
+	require.NoError(t, err)
+	require.NotNil(t, consumer.onHandlerError)
+	consumer.onHandlerError(context.Background(), redis.XMessage{ID: "1-0"}, context.Canceled)
+	require.True(t, called)
 }
